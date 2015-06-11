bd52d04c1e57c603e1094454fe2958f26f6d489a
Diego Malagón <diegomalagonh@gmail.com>
2015-06-11 17:29:51 -0500

    Implementación de modalbox, Cambio de texto de modal, Cambio de idioma de los botones

--- js/widget.js
+++ js/widget.js
@@ -18,15 +18,25 @@ jQuery(document).ready(function($){
         else{
             var url = $link.attr("href");
 
-            var text = "are you sure to download this file?";
+            var text = "You are about to download confidential information. You may only download this file once. If you need to download it again, please contact tekapef@tekacap.com";
+            
+            modal({
+				type: 'confirm',
+				title: 'Warning',
+				text: text,
+                buttonText: {
+					yes: 'Accept',
+					cancel: 'Cancel'
+				},
+				callback: function(result) {
+					if(result){
+                        $link.addClass('locked');
+                        $link.attr("href", "#");
 
-            if(confirm(text)){
-                
-                $link.addClass('locked');
-                $link.attr("href", "#");
-                
-                window.location = url;
-            }
+                        window.location = url;
+                    }
+				}
+			});
         }
     });
 });
